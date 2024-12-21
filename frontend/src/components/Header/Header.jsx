504170e55469e774367e3de6d9852d2a76e6860f
Shehan Sanjula <116501697+ShehanSanjula123@users.noreply.github.com>
2024-12-21 23:54:23 +0530

    edit header with adding navbar part

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Container, Row, Button} from 'reactstrap'
 import { NavLink, Link } from 'react-router-dom'
+import logo from '../../assets/images/logo.png'
+import '../Header/header.css'
 
 const nav_links=[
   {
@@ -22,9 +24,34 @@ const Header = () => {
  <header className='header'>
   <Container>
     <Row>
-      <dev className='nav_wrapper d-flex align-items-center justify-content-between'>
+      <div className='nav_wrapper d-flex align-items-center justify-content-between'>
+         <div className='logo'>
+          <img src={logo} alt=''/>
+         </div>
 
-      </dev>
+         <div className='navigation'>
+          <ul className='menu d-flex align-items-center gap-5'>
+            {nav_links.map((item,index) => (
+                <li className='nav_item' key={index}>
+                  <NavLink to={item.path}>{item.display}</NavLink>
+                </li>
+              ))}
+          </ul>
+         </div>
+
+         <div className='nav_right d-flex align-items-center gap-4'>
+          <div className='nav_btns d-flex align-items-center gap-4'>
+               <Button className='btn secondary_btn'><Link to='/login'>Login</Link></Button>
+               <Button className='btn primary_btn'><Link to='/register'>Register</Link> </Button>
+          </div>
+
+          <span className='mobile_menu'>
+          <i class="ri-menu-line"></i>
+          </span>
+
+         </div>
+
+      </div>
     </Row>
   </Container>
  </header>
